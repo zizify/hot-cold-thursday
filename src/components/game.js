6507c006766cea3e1cc18b89feadaf2c7c7e38c1
Andy Gaines <andyagaines@gmail.com>
2017-11-09 11:25:45 -0600

    got count incrementing

--- src/components/game.js
+++ src/components/game.js
@@ -5,14 +5,37 @@ import GuessSection from './guess-section';
 import GuessCount  from './guess-count';
 import GuessList from './guess-list';
 
-export default function Game(props) {
-    return (
-        <div>
-            <Header />
-            <GuessSection feedback="Make your guess!" />
-            <GuessCount count={3} />
-            <GuessList guesses={[10, 15, 25]} />
-        </div>
-    );
+export default class Game extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            answer: null,
+            currentGuess: null,
+            currentFeedback: 'Make a guess!',
+            feedback: ['hot', 'warm', 'cool', 'cold', 'You won!'],
+            count: 0,
+            pastGuesses: [],
+            whatView: false
+        };
+
+        this.incrementCount = this.incrementCount.bind(this);
+    }
+
+    incrementCount() {
+        // console.log(this.state);
+        let newCount = this.state.count + 1
+        this.setState({count: newCount})
+    }
+
+    render() {
+        return (
+            <div>
+                <Header />
+                <GuessSection feedback={this.state.currentFeedback} increment={this.incrementCount}/>
+                <GuessCount count={this.state.count} />
+                <GuessList guesses={this.state.pastGuesses} />
+            </div>
+        );
+    }
 }
 
--- src/components/guess-form.js
+++ src/components/guess-form.js
@@ -3,12 +3,13 @@ import React from 'react';
 import './guess-form.css';
 
 export default function GuessForm(props) {
+    console.log(props);
     return (
         <form>
             <input type="text" name="userGuess" id="userGuess"
                 className="text" maxLength="3" autoComplete="off"
-                placeholder="Enter your Guess" required />
-            <input type="submit" id="guessButton" className="button" name="submit" value="Guess"/>
+                placeholder="Enter your Guess" />
+            <input type="submit" id="guessButton" onClick={e => {e.preventDefault; props.increment()}} className="button" name="submit" value="Guess"/>
         </form>
     );
 };
